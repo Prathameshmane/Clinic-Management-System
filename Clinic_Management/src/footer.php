b24d8bc4a740737e668319c770459563872024f3
Prathamesh Mane <46131897+Prathameshmane@users.noreply.github.com>
2020-02-13 19:33:32 +0530

    first commit

---
+++ Clinic_Management/src/footer.php
@@ -0,0 +1,78 @@
+<footer id="footer">
+    <!-- Start Footer Top -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class="section-heading">
+                            <h2>About Us</h2>
+                            <div class="line"></div>
+                        </div>
+                        <p>Homeopathy cures a larger percentage of cases than any other form of treatment and is beyond doubt safer and more economical.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class="section-heading">
+                            <h2>Our Service</h2>
+                            <div class="line"></div>
+                        </div>
+                        <ul class="footer-service">
+                            <li><a><span class="fa fa-check"></span>Family Doctor</a></li>
+                            <li><a><span class="fa fa-check"></span>Paediatric Clinic</a></li>
+                            <li><a><span class="fa fa-check"></span>Cardiology Clinic</a></li>
+                            <li><a><span class="fa fa-check"></span>Gynaecology & Obestrics</a></li>
+                            <li><a><span class="fa fa-check"></span>Rheumatology</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class=" section-heading">
+                            <h2>Contact Info</h2>
+                            <div class="line"></div>
+                        </div>
+                        <p>Feel free to contact us any moment using any of the details provided below.</p>
+                        <address class="contact-info">
+                            <p><span class="fa fa-home"></span>Sapale's Polyclinic, Malik Building, D V Deshpande Marg, Behind Oven-fresh cake shop, Dadar West, Shivaji Park
+Mumbai, Maharashtra 400016, <i>India</i></p>
+                            <p><span class="fa fa-phone"></span>9876543210</p>
+                            <p><span class="fa fa-envelope"></span>Vinodsawant@gmail.com</p>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Start Footer Middle -->
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-copyright">
+                        <?php echo "<p>&copy; Copyright 2020-" . date("Y") . " <a href=\"index.html\">Shiksha AI & Robotics</a></p>"; ?>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-social">
+                        <a href="https://www.facebook.com/imidiotic12"><span class="fa fa-facebook"></span></a>
+                        <a href="https://twitter.com/piyushbhutoria9"><span class="fa fa-twitter"></span></a>
+                        <a href="https://github.com/Piyushhbhutoria"><span class="fa fa-github"></span></a>
+                        <a href="https://www.linkedin.com/in/piyushh-bhutoria-for-you/"><span class="fa fa-linkedin"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Start Footer Bottom -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p>Design & Developed By <a href="https://www.techshiksha.net/" target="_blank">Shiksha AI & Robotics</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ Clinic_Management/src/slider.php
@@ -0,0 +1,36 @@
+<section id="sliderArea">
+    <!-- Start slider wrapper -->
+    <div class="top-slider">
+        <!-- Start First slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/14.jpg">
+            </div>
+            <div class="slider-text">
+                <h2>An <strong>Excellent Theme</strong> for your Health Related Project</h2>
+                <p><strong>SPA Pathcare</strong> Theme suitable for any health and medical related projets or business
+                </p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="Features.php"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End First slide -->
+
+        <!-- Start Second slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img alt="" src="images/7.jpg">
+            </div>
+            <div class="slider-text">
+                <h2>An <strong>Excellent Theme</strong> for your Health Related Project</h2>
+                <p><strong>SPA Pathcare</strong> Theme suitable for any health and medical related projets or business
+                </p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End Second slide -->
+    </div><!-- /top-slider -->
+</section>
